1a2915d8123f2cd6ae3e1296618d37a7d72d7b8a
PatrickTomas2 <patpat.john.tomas02@gmail.com>
2024-04-16 18:46:38 +0800

    push

--- config.php
+++ config.php
@@ -2,7 +2,7 @@
     $localhost = 'localhost';
     $username = 'root';
     $password = '';
-    $database = 'business_permit_sample';
+    $database = 'business_permit_sample_new';
 
     $conn = mysqli_connect($localhost, $username, $password, $database) or die ('db error in connecting: '.mysqli_connect_error());
     $conn->set_charset('utf8');
